0ae415cd278bd043ffc5306f1edaa266b8709035
LinogeFly <LinogeFly@users.noreply.github.com>
2016-04-20 17:13:51 +0300

    Added draft version of language bar component.

---
+++ src/js/app/language-bar.js
@@ -0,0 +1,122 @@
+﻿(function () {
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    var observer = new MutationObserver(function (mutations) {
+        addLanguageBar(mutations);
+    });
+
+    function addLanguageBar(mutations) {
+        var $header = getHeader(mutations);
+        if (!$header.length)
+            return;
+
+        stop();
+        (new LanguageBar()).init($header);
+        start();
+    }
+
+    function getHeader(mutations) {
+        var selector = '.section_header.first > .title';
+
+        if (!isNodeAdded(mutations, selector))
+            return $();
+
+        return $(selector);
+    }
+
+    function isNodeAdded(mutations, selector) {
+        return mutations.some(function (item) {
+            return $(item.addedNodes).find(selector).length !== 0;
+        });
+    }
+
+    function start() {
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    }
+
+    function stop() {
+        observer.disconnect();
+    }
+
+    start();
+})();
+
+var LanguageBar = function (storage) {
+    this.storage = storage;
+
+    /*jshint multistr: true */
+    this.layout = '\
+        <div class="tlf-languageBar">\
+            <a href="javascript:void(0);" class="tlf-languageBar-current">EN</a>\
+            <ul class="tfl-languageBar-menu" style="display: none;">\
+                <li data-code="en">English</li>\
+                <li data-code="da">Dansk</li>\
+                <li data-code="de">Deutsch</li>\
+                <li data-code="es">Español</li>\
+                <li data-code="fr">Français</li>\
+                <li data-code="it">Italiano</li>\
+                <li data-code="hu">Magyar</li>\
+                <li data-code="nl">Nederlands</li>\
+                <li data-code="no">Norsk</li>\
+                <li data-code="pl">Polski</li>\
+                <li data-code="pt">Português</li>\
+                <li data-code="sk">Slovenčina</li>\
+                <li data-code="fi">Suomi</li>\
+                <li data-code="sv">Svenska</li>\
+                <li data-code="vi">Tiếng Việt</li>\
+                <li data-code="tr">Türkçe</li>\
+                <li data-code="cs">Čeština</li>\
+                <li data-code="bg">Български</li>\
+                <li data-code="ru">Русский</li>\
+                <li data-code="ar">العربية</li>\
+                <li data-code="th">ภาษาไทย</li>\
+                <li data-code="zh">中文</li>\
+                <li data-code="ja">日本語</li>\
+                <li data-code="ko">한국어</li>\
+            </ul>\
+        </div>\
+    ';
+};
+
+LanguageBar.prototype = (function() {
+    function init ($insertBeforeElem) {
+        var self = this;
+
+        // Create component
+        $container = $(self.layout);
+        $menu = $container.find('.tfl-languageBar-menu');
+        $button = $container.find('.tlf-languageBar-current');
+        $langItems = $menu.find('li');
+
+        $button.click(function (e) {
+            e.stopPropagation();
+            $menu.toggle();
+        });
+
+        $menu.click(function (e) {
+            e.stopPropagation();
+        });
+
+        $(document).click(function () {
+            $menu.hide();
+        });
+
+        $langItems.click(function () {
+            $menu.hide();
+            language_changed();
+        });
+
+        // Add component
+        $insertBeforeElem.before($container);
+    }
+
+    function language_changed() {
+        console.log('Language changed');
+    }
+
+    return {
+        init: init
+    };
+})();
--- src/js/app/layouts.js
+++
@@ -1,8 +0,0 @@
-﻿/*jshint multistr: true */
-var layouts = layouts || {
-    languageBar: '\
-        <div class="tlf-languageBar">\
-            <a href="javascript:void(0);" class="tlf-languageBar-current">EN</a>\
-        </div>\
-    '
-};
