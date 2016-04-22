e7b9e532c60cfaaef9c0792cb0a1fce318304297
LinogeFly <LinogeFly@users.noreply.github.com>
2016-04-22 23:12:57 +0300

    Added gulp task to run jasmine tests along with few empty tests for UI.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,15 @@ var gulp = require('gulp'),
     footer = require('gulp-footer'),
     del = require('del'),
     merge = require('merge-stream'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    jasmine = require('gulp-jasmine');
+
+function getAppStream() {
+    return gulp.src(['src/js/app/**/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('app.js'));
+}
 
 gulp.task('clean', function () {
     return del(['build']);
@@ -22,10 +30,7 @@ gulp.task('css', ['clean'], function () {
 });
 
 gulp.task('js', ['clean'], function () {
-    var app = gulp.src(['src/js/app/**/*.js'])
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .pipe(concat('app.js'))
+    var app = getAppStream()
         .pipe(header('(function(global) {'))
         .pipe(footer('main();}(window.TwitchLanguageFilter = window.TwitchLanguageFilter || {}));'));
 
@@ -39,12 +44,22 @@ gulp.task('js', ['clean'], function () {
         .pipe(gulp.dest('build'));
 });
 
+gulp.task('js:test', ['clean'], function () {
+    return getAppStream()
+        .pipe(gulp.dest('build'));
+});
+
 gulp.task('watch', ['default'], function () {
     gulp.watch('src/**/*.*', ['default']);
 });
 
 gulp.task('dev', ['js', 'css', 'vendor'], function () {
 
+});
+
+gulp.task('test', ['js:test'], function () {
+    return gulp.src(['test/**/*.js'])
+        .pipe(jasmine());
 });
 
 gulp.task('default', ['dev'], function () {
---
+++ test/ui.spec.js
@@ -0,0 +1,13 @@
+﻿describe("UI initializer", function () {
+    it("adds LanguageBar on allowed pages", function () {
+
+    });
+
+    it("doesn't add LanguageBar on not allowed pages", function () {
+
+    });
+
+    it("adds LanguageBar only once", function () {
+
+    });
+});
