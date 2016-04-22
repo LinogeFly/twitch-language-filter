9e97dd643c245ac62a8fb7cd73cd9ea4445c2180
LinogeFly <LinogeFly@users.noreply.github.com>
2016-04-22 17:10:39 +0300

    Added string format helper.

---
+++ src/js/app/helpers.js
@@ -0,0 +1,8 @@
+﻿if (!String.format) {
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : match;
+        });
+    };
+};
