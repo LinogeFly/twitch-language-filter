e08eebf6dfb93945cb58b6df7f8cc9ccb9b56415
LinogeFly <LinogeFly@users.noreply.github.com>
2016-04-29 19:58:13 +0300

    Added Log module for logging.

---
+++ src/js/app/log.js
@@ -0,0 +1,16 @@
+﻿var config = require('./config');
+
+module.exports = {
+    error: function (message) {
+        if (config.logLevel.toLowerCase() != "error")
+            return;
+
+        console.log('TFL ERROR: ' + message);
+    },
+    debug: function (message) {
+        if (config.logLevel.toLowerCase() === "none")
+            return;
+
+        console.log('TFL DEBUG: ' + message);
+    }
+};
---
+++ test/log.spec.js
@@ -0,0 +1,68 @@
+﻿describe("Log module", function () {
+    var rewire = require("rewire");
+    var log = rewire('../src/js/app/log.js');
+
+    function setLogLevel(level) {
+        log.__set__('config', {
+            logLevel: level
+        });
+    };
+
+    describe("error()", function () {
+        it("shouldn't write if level is 'Verbose'", function () {
+            setLogLevel('Verbose');
+            spyOn(console, 'log');
+
+            log.error('Test message');
+
+            expect(console.log.calls.count()).toBe(0);
+        });
+
+        it("should write if level is 'Error'", function () {
+            setLogLevel('Error');
+            spyOn(console, 'log');
+
+            log.error('Test message');
+
+            expect(console.log).toHaveBeenCalled();
+        });
+
+        it("shouldn't write if level is 'None'", function () {
+            setLogLevel('None');
+            spyOn(console, 'log');
+
+            log.error('Test message');
+
+            expect(console.log.calls.count()).toBe(0);
+        });
+    });
+
+    describe("debug()", function () {
+        it("should write if level is 'Verbose'", function () {
+            setLogLevel('Verbose');
+            spyOn(console, 'log');
+
+            log.debug('Test message');
+
+            expect(console.log).toHaveBeenCalled();
+        });
+
+        it("should write if level is 'Error'", function () {
+            setLogLevel('Error');
+            spyOn(console, 'log');
+
+            log.debug('Test message');
+
+            expect(console.log).toHaveBeenCalled();
+        });
+
+        it("shouldn't write if level is 'None'", function () {
+            setLogLevel('None');
+            spyOn(console, 'log');
+
+            log.debug('Test message');
+
+            expect(console.log.calls.count()).toBe(0);
+        });
+    });
+});
