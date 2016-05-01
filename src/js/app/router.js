821caab35dd10c274e7ee8762b71356e69405cd9
LinogeFly <LinogeFly@users.noreply.github.com>
2016-05-02 00:06:58 +0300

    Added initial empty version of Router module.

---
+++ src/js/app/router.js
@@ -0,0 +1,28 @@
+﻿var log = require('./log.js');
+
+var Router = function () { };
+
+Router.prototype = {
+    _routes: [
+        {
+            name: 'game-directory.index',
+            endpoint: 'streams'
+        },
+        {
+            name: 'channels.all',
+            endpoint: 'streams'
+        },
+        {
+            name: 'channels.random',
+            endpoint: 'beta/streams/random'
+        }
+    ],
+    isRouteSupported: function () {
+
+    },
+    getEndpoint: function () {
+
+    }
+};
+
+module.exports = Router;
