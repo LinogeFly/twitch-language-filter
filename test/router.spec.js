e18fcbfbf2000e9c03e0883b002641bf546845d1
LinogeFly <LinogeFly@users.noreply.github.com>
2016-05-03 00:18:17 +0300

    Added empty tests for Router.

--- test/router.spec.js
+++ test/router.spec.js
@@ -1,1 +1,25 @@
-﻿
+﻿describe("Router", function () {
+    it("_getCurrentRouteName() should prioritize TargetRoute value over CurrentRoute", function () {
+
+    });
+
+    it("_getCurrentRouteName() should throw error if route cannot be fetched", function () {
+
+    });
+
+    it("isRouteSupported() should return 'true' when route match is found", function () {
+
+    });
+
+    it("isRouteSupported() should return 'false' when route match isn't found", function () {
+
+    });
+
+    it("getRequestUrlRegExp() should return value when route is supported", function () {
+
+    });
+
+    it("isRouteSupported() shouldn return 'undefined' when route isn't supported", function () {
+
+    });
+});
