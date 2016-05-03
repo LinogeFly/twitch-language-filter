5d936f3dc345007a8bf09d13761b79c1ac426736
LinogeFly <LinogeFly@users.noreply.github.com>
2016-05-03 12:41:36 +0300

    Implemented tests for Router module.

--- test/router.spec.js
+++ test/router.spec.js
@@ -1,25 +1,75 @@
 ﻿describe("Router", function () {
-    it("_getCurrentRouteName() should prioritize TargetRoute value over CurrentRoute", function () {
+    var rewire = require("rewire"),
+        Router;
 
+    beforeEach(function () {
+        Router = rewire('../src/js/app/router.js');
+        require('../src/js/core/array-helpers.js');
+        require('./helpers/module-setup.js')(Router);
+    });
+
+    it("_getCurrentRouteName() should prioritize TargetRoute value over CurrentRoute", function () {
+        var router = new Router();
+        spyOn(router, '_getTwitchRouter').and.returnValue({
+            currentRouteName: 'following.index',
+            targetState: {
+                routerJs: {
+                    activeTransition: {
+                        targetName: 'channels.all'
+                    }
+                }
+            }
+        });
+        
+       var result = router._getCurrentRouteName();
+
+       expect(result).toBe('channels.all');
     });
 
     it("_getCurrentRouteName() should throw error if route cannot be fetched", function () {
+        var router = new Router();
+        spyOn(router, '_getTwitchRouter').and.returnValue({});
 
+        expect(router._getCurrentRouteName).toThrowError();
     });
 
     it("isRouteSupported() should return 'true' when route match is found", function () {
+        var router = new Router();
+        spyOn(router, '_getCurrentRouteName').and.returnValue('channels.all');
+        router._routes = [
+            { name: 'channels.all', url: '^https?://streams.twitch.tv/kraken/streams/?$' }
+        ];
 
+        expect(router.isRouteSupported()).toBe(true);
     });
 
     it("isRouteSupported() should return 'false' when route match isn't found", function () {
+        var router = new Router();
+        spyOn(router, '_getCurrentRouteName').and.returnValue('following.index');
+        router._routes = [
+            { name: 'channels.all', url: '^https?://streams.twitch.tv/kraken/streams/?$' }
+        ];
 
+        expect(router.isRouteSupported()).toBe(false);
     });
 
     it("getRequestUrlRegExp() should return value when route is supported", function () {
+        var router = new Router();
+        spyOn(router, '_getCurrentRouteName').and.returnValue('channels.all');
+        router._routes = [
+            { name: 'channels.all', url: '^https?://streams.twitch.tv/kraken/streams/?$' }
+        ];
 
+        expect(router.getRequestUrlRegExp()).toBe('^https?://streams.twitch.tv/kraken/streams/?$');
     });
 
-    it("isRouteSupported() shouldn return 'undefined' when route isn't supported", function () {
+    it("getRequestUrlRegExp() should return 'undefined' when route isn't supported", function () {
+        var router = new Router();
+        spyOn(router, '_getCurrentRouteName').and.returnValue('following.index');
+        router._routes = [
+            { name: 'channels.all', url: '^https?://streams.twitch.tv/kraken/streams/?$' }
+        ];
 
+        expect(router.getRequestUrlRegExp()).toBe(undefined);
     });
 });
